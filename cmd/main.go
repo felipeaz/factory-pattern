61d7c882cbb5eea41e686277a5e3542a7ff5a9aa
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2022-12-12 07:51:27 -0300

    Added pizza store call in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "factory-pattern/internal/domain/pizzaStore"
+import (
+	"log"
+
+	"factory-pattern/internal/domain/pizza"
+	"factory-pattern/internal/domain/pizzaStore"
+)
 
 func main() {
 	brazilianPizzaStore := pizzaStore.NewBrazilianPizzaStore()
 	brazilianPizzaStore.StartWork()
+
+	err := brazilianPizzaStore.Order(pizza.BrazilianCheesePizzaTitle)
+	if err != nil {
+		log.Fatal("failed to order pizza", err)
+	}
+
+	err = brazilianPizzaStore.Order(pizza.BrazilianPepperoniPizzaTitle)
+	if err != nil {
+		log.Fatal("failed to order pizza", err)
+	}
 }
